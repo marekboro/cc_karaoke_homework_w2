26aa5a2d1148cb8b1a90299c6f5733c3a092ab0b
marekboro <marek.borowicz83@gmail.com>
2020-07-26 21:22:03 +0100

    refactored a lot in the kareoke_bar class and test file, added a checking funciton to the kareokebar, which is probably where it should have been from the scratch, created a test where I add a customer to an empty room and check for the name, this failed correctly as it was setup to fail

--- classes/karaoke_bar.py
+++ classes/karaoke_bar.py
@@ -1,8 +1,7 @@
 class KaraokeBar:
-    def __init__(self,rooms,has_available_rooms ,till,songsDB):
+    def __init__(self,rooms,till,songsDB):
         self.name = "Singoholics Annonymous"
         self.rooms = rooms
-        self.has_available_rooms = has_available_rooms
         self.till = till
         self.songsDB = songsDB
     
@@ -10,5 +9,9 @@ class KaraokeBar:
     def get_name(self):
         bar_name = self.name
         return bar_name
+
+    def check_in(self,room_number, customer):
+        self.rooms[room_number].check_in_to_room(customer,room_number)
+
     #def count_available_rooms()
 
--- tests/karaoke_bar_test.py
+++ tests/karaoke_bar_test.py
@@ -1,16 +1,46 @@
 import unittest
 from classes.karaoke_bar import KaraokeBar
+from classes.song import Song
+from classes.guest import Guest
+from classes.room import Room
+
 
 class TestKaraokeBar(unittest.TestCase):
 
     def setUp(self):
-        rooms = [1,2,3,4]
-        money_in_till = 100
-        are_rooms_available = True
-        songs_database = ["Radio GaGa","We are the champions","Under Pressure"]
-        self.the_bar = KaraokeBar(rooms,are_rooms_available, money_in_till,songs_database)
+        room_1 = Room(1,[],False,None,300)
+        room_2 = Room(2,[],False,None,300)
+        room_3 = Room(3,[],False,None,300)
+        room_4 = Room(4,[],False,None,300)
+        room_5 = Room(5,[],False,None,300)
 
+        self.Bar_rooms = [room_1,room_2,room_3,room_4,room_5]
+        
+        song_1 = Song("Song 1", "indie_rock")
+        song_2 = Song("Song 2","pop")
+        song_3 = Song("Song 3","rock")
+        song_4 = Song("Song 4","rock")
+        song_5 = Song("Song 5","rock")
+        song_6 = Song("Song 6","disco")
+        song_7 = Song("Song 7","disco")
+        song_8 = Song("Song 8","pop")
+        song_9 = Song("Song 9","funk")
+        song_10 = Song("Song 10","funk")
+        
+        self.songs_database = [song_1,song_2,song_3,song_4,song_5,song_6,song_7,song_8,song_9,song_10]
+        
+        self.money_in_till = 200
+        
+        self.the_bar = KaraokeBar(self.Bar_rooms, self.money_in_till, self.songs_database)
 
+        self.customer_1 = Guest("Mr Curst Omar1",600, song_8)
+        self.customer_2 = Guest("Mr Curst Omar2",400, song_1)
+        self.customer_3 = Guest("Mr Curst Omar3",500, song_2)
+        self.customer_4 = Guest("Mr Curst Omar4",290, song_3)
+        self.customer_5 = Guest("Mr Curst Omar5",310, song_5)
+        self.customer_6 = Guest("Mr Curst Omar6",400, song_6)
+        self.customer_7 = Guest("Mr Curst Omar7",400, song_9)
+        #self.check_in = KaraokeBar.check_in
     #def test_bar_has_a_name(self):
 
    #     pass
@@ -19,3 +49,15 @@ class TestKaraokeBar(unittest.TestCase):
         bar_name = self.the_bar.get_name()
         actual = bar_name
         self.assertEqual(expected,actual)
+
+    def test_checking_name_after_customer__checks_into_a_room_(self):
+        expected = "hello"
+        the_customer = self.customer_1
+        self.the_bar.check_in(0,the_customer)
+        #[1].check_in_to_room(customer_1)
+        
+        customer_name = self.the_bar.rooms[0].occupant.name
+        actual = customer_name
+        self.assertEqual(expected,actual)
+
+    
